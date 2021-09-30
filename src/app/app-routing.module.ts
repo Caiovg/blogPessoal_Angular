d846221acf81af26a2df12877d839333412bb7ff
unknown <caioviniciusg.1000@gmail.com>
2021-09-30 12:04:08 -0300

    Componente tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'login', pathMatch:'full'},
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastrarComponent},
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
