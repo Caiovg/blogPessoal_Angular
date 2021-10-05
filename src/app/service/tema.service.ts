a6f6f4aefceecbd481e594ba48f9ad55dda0134e
unknown <caioviniciusg.1000@gmail.com>
2021-10-05 11:13:25 -0300

    Implementação de put e delete de tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -17,7 +17,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalcopy.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalcopy.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalcopy.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema:Tema):Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoalcopy.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessoalcopy.herokuapp.com/tema/${id}`, this.token)
   }
 }
