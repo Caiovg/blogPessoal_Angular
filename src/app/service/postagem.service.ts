88e9171d33eefd9ef33560a988ce7ed8fe800d9c
unknown <caioviniciusg.1000@gmail.com>
2021-10-06 12:35:30 -0300

    Implementação do put de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -17,7 +17,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogpessoalcopy.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoalcopy.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoalcopy.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoalcopy.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://blogpessoalcopy.herokuapp.com/postagens/${id}`, this.token)
   }
 }
