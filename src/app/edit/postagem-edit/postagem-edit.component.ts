f04e1a18f83e5463be4996429c54bf6d1809d046
unknown <caioviniciusg.1000@gmail.com>
2021-10-06 14:10:49 -0300

    Implementação do delete de postagem

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -25,6 +25,8 @@ export class PostagemEditComponent implements OnInit {
   ) { }
 
   ngOnInit(){
+    window.scroll(0,0)
+    
     if(environment.token == ''){
       this.router.navigate(['/login'])
     }
